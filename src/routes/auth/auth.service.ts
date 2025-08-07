922e2749bd710f2c60689ea24edf4abe0d3e25b4
TamTranZrgz <thanh.tam.tran.zrgz@gmail.com>
2025-08-07 23:51:33 +0200

    Register - update verification code for register function

--- src/routes/auth/auth.service.ts
+++ src/routes/auth/auth.service.ts
@@ -8,6 +8,7 @@ import { SharedUserRepository } from 'src/shared/repositories/shared-user.repo'
 import { addMilliseconds } from 'date-fns'
 import ms from 'ms'
 import envConfig from 'src/shared/config'
+import { TypeOfVerificationCode } from 'src/shared/constants/auth.constant'
 
 @Injectable()
 export class AuthService {
@@ -20,8 +21,38 @@ export class AuthService {
 
   async register(body: RegisterBodyType) {
     try {
+      // 1. Verify OTP code
+      const verificationCode = await this.authRepository.findUniqueVerificationCode({
+        email: body.email,
+        code: body.code,
+        type: TypeOfVerificationCode.REGISTER,
+      })
+
+      if (!verificationCode) {
+        throw new UnprocessableEntityException([
+          {
+            path: 'code',
+            message: 'Invalid OTP code',
+          },
+        ])
+      }
+
+      if (verificationCode.expiresAt < new Date()) {
+        throw new UnprocessableEntityException([
+          {
+            path: 'code',
+            message: 'OTP code has expired',
+          },
+        ])
+      }
+
+      // 2. Get client role
       const clientRoleId = await this.rolesService.getClientRoleId()
+
+      // 3. Hash password
       const hashedPassword = await this.hashingService.hash(body.password)
+
+      // 4. Create new user in db
       const user = await this.authRepository.createUser({
         email: body.email,
         name: body.name,
